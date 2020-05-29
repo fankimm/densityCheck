efa693920c1c8bd7f34628fa8ca5f6e75279a5c4
fankimm <crakim7@naver.com>
2020-05-29 12:16:10 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+let d
+
+function setup(){
+  d = pixelDensity()
+  createCanvas(500,500)
+  text('density = '+d,100,100)
+  text('sW = '+screen.width,100,110)
+  text('sH = '+screen.height,100,120)
+
+}
+function draw(){
+
+}
